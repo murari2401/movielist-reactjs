4ca4005ae82a92141ac19f2c0f46f9aade63731e
murari2401 <iammurari89@gmail.com>
2024-07-06 23:59:14 +0530

    added link to each card so they can go to google search

--- movie-vault/src/components/MovieCard.jsx
+++ movie-vault/src/components/MovieCard.jsx
@@ -2,17 +2,19 @@ import React from "react";
 
 function MovieCard({poster,name}) {
   return (
-    <div
-      className="h-[40vh] w-[200px] bg-center bg-cover rounded-xl hover:cursor-pointer hover:scale-110 duration-300 flex flex-col-reverse"
-      style={{
-        backgroundImage:
-          `url(https://image.tmdb.org/t/p/original/${poster})`
-      }}
-    >
-      <div className="text-white text-xl w-full p-2 text-center bg-gray-900/60">
-        {name}
+    <a href={`https://www.google.com/search?q=${name}`} target="_blank" rel="noopener noreferrer">
+        <div
+        className="h-[40vh] w-[200px] bg-center bg-cover rounded-xl hover:cursor-pointer hover:scale-110 duration-300 flex flex-col-reverse"
+        style={{
+          backgroundImage:
+            `url(https://image.tmdb.org/t/p/original/${poster})`
+        }}
+      >
+        <div className="text-white text-xl w-full p-2 text-center bg-gray-900/60">
+          {name}
+        </div>
       </div>
-    </div>
+    </a>
   );
 }
 
