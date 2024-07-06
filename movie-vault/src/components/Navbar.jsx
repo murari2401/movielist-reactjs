bb53f4c5386b9bfe4b8ffb402a4d4dbf0dd5268a
murari2401 <iammurari89@gmail.com>
2024-07-07 00:04:02 +0530

    added link on logo

--- movie-vault/src/components/Navbar.jsx
+++ movie-vault/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 function Navbar() {
   return (
     <div className='flex items-center p-3'>
-        <img className='h-[50px] mx-6' src={logoimg} alt="" />
+        <Link to='/'><img className='h-[50px] mx-6 ' src={logoimg} alt="" /></Link>
         <Link to="/" className='text-3xl mx-6 font-bold text-sky-600 hover:cursor-pointer'>Movies</Link>
         <Link to="/watchlist" className='text-3xl mx-6 font-bold text-sky-600 hover:cursor-pointer'>Watchlist</Link>
     </div>
