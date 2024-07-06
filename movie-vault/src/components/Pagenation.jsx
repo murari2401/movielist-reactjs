d09977dcd951179c886edf7f18870b71f954e42d
murari2401 <iammurari89@gmail.com>
2024-07-06 23:58:47 +0530

    addressed a mistake

--- movie-vault/src/components/Pagenation.jsx
+++ movie-vault/src/components/Pagenation.jsx
@@ -4,13 +4,13 @@ function Pagenation({page,incrementPage,decrementPage}) {
   return (
     <div className='bg-gray-400 p-4 mt-8 flex justify-center'>
         <div className='px-8 hover:cursor-pointer'>
-            <button onClick={decrementPage}><i class="fa-solid fa-arrow-left"></i></button>
+            <button onClick={decrementPage}><i className="fa-solid fa-arrow-left"></i></button>
         </div>
 
         <div className='font-bold'>{page}</div>
 
         <div className='px-8 hover:cursor-pointer'>
-            <button onClick={incrementPage}><i class="fa-solid fa-arrow-right"></i></button>
+            <button onClick={incrementPage}><i className="fa-solid fa-arrow-right"></i></button>
         </div>
     </div>
   )
