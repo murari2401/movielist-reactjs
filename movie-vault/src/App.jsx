70a5d36fd64c85b3474ec268604b265906e54a85
murari2401 <iammurari89@gmail.com>
2024-07-06 16:42:19 +0530

    added routing

--- movie-vault/src/App.jsx
+++ movie-vault/src/App.jsx
@@ -1,16 +1,32 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Movies from "./components/Movies";
+import Watchlist from "./components/Watchlist";
+import Banner from "./components/Banner";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import './App.css'
-
 function App() {
   return (
     <>
       <div>
-        <h1>hello..</h1>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route
+              element={
+                <>
+                  <Banner/>
+                  <Movies />
+                </>
+              }
+              path="/"
+            ></Route>
+            <Route element={<Watchlist />} path="/watchlist"></Route>
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
