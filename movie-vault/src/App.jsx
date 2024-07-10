3f383498e3da81b2834ecee5a3ecb3415dbf1935
murari2401 <iammurari89@gmail.com>
2024-07-10 09:31:24 +0530

    added watchlist func & localstorage impl

--- movie-vault/src/App.jsx
+++ movie-vault/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Movies from "./components/Movies";
 import Watchlist from "./components/Watchlist";
@@ -6,6 +6,28 @@ import Banner from "./components/Banner";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import './App.css'
 function App() {
+  let [watchList, setwatchList] = useState([]);
+  let handleTAddToWathchList = (movieObj) => {
+    let newWatchList = [...watchList, movieObj];
+    localStorage.setItem("moviesApp", JSON.stringify(newWatchList));
+    setwatchList(newWatchList);
+    // console.log(newWatchList);
+  };
+  let handleDelFromWatchList = (movieObj) => {
+    let filterdwatchList = watchList.filter((movie) => {
+      return movie.id != movieObj.id;
+    });
+    localStorage.setItem("moviesApp", JSON.stringify(filterdwatchList));
+    setwatchList(filterdwatchList);
+    // console.log(filterdwatchList);
+  };
+  useEffect(() => {
+    let moviesFromLocalStorage = localStorage.getItem("moviesApp");
+    // console.log(moviesFromLocalStorage)
+    if (moviesFromLocalStorage) {
+      setwatchList(JSON.parse(moviesFromLocalStorage));
+    }
+  }, []);
   return (
     <>
       <div>
@@ -15,13 +37,20 @@ function App() {
             <Route
               element={
                 <>
-                  <Banner/>
-                  <Movies />
+                  <Banner />
+                  <Movies
+                    handleTAddToWathchList={handleTAddToWathchList}
+                    watchList={watchList}
+                    handleDelFromWatchList={handleDelFromWatchList}
+                  />
                 </>
               }
               path="/"
             ></Route>
-            <Route element={<Watchlist />} path="/watchlist"></Route>
+            <Route
+              element={<Watchlist watchList={watchList} />}
+              path="/watchlist"
+            ></Route>
           </Routes>
         </BrowserRouter>
       </div>
